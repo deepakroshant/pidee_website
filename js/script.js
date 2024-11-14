2eed7f9790b28cb54121a990e84ce7f9eb64e585
Deepak Thiagarajan <deepakroshan73@gmail.com>
2024-11-13 19:03:50 -0800

    Added basic tempelate

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// JavaScript to add functionality
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Website loaded successfully');
+    // Add any interactive functionality here
+});
